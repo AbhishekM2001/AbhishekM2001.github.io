0fcab87b32ee17ee347acc73941f69848c187bec
Abhishek Mukherjee <mukherjeea2017@gmail.com>
2022-06-29 21:45:18 +0530

    Full portfolio app code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Projects from './Projects'
+import SocialProfiles from "./SocialProfiles";
+import pfp from './assets/profile.png'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    state = { displayBio: false }
+    // constructor() {
+    //     super();
+    //     this.state = { displayBio: false }
+    //     this.togglebio = this.togglebio.bind(this)
+    // }
+
+    togglebio = () => {
+        this.setState({ displayBio: !this.state.displayBio })
+    }
+
+    render() {
+        return (
+            <div>
+                <img src={pfp} alt='profile' className="pfp" />
+                <h1> Hi Abhishek here</h1>
+                <p>I aspire to be a software engineer.
+                    My first goal towards this would be to get good grades and make a stand out resume.
+                </p>
+                {this.state.displayBio ? (
+                    <div>
+                        I live in Mumbai and I'm learning ReactJS which is a JavaScript frontend framework
+                        <div>
+                            <button onClick={this.togglebio}>Show Less</button>
+                        </div>
+                    </div>
+                ) :
+                    (
+                        <div>
+                            <button onClick={this.togglebio}>Read More</button>
+                        </div>
+                    )
+                }
+
+                <hr />
+                <Projects />
+                <hr />
+                <SocialProfiles />
+
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
