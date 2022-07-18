3c29e00b4df3f87dbd3bb99a9011cdd1673cc1a5
Abhishek Mukherjee <mukherjeea2017@gmail.com>
2022-07-18 23:40:27 +0530

    Updated till Header

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './app'
+import createBrowserHistory from "history/createBrowserHistory";
+
+import { Router, Switch, Route } from 'react-router-dom'
+import App from './Components/app'
+import Jokes from './Components/Jokes'
 import './index.css'
 
-ReactDOM.render(<App />, document.querySelector('#root'))
+const history = createBrowserHistory()
+ReactDOM.render(
+    <Router history={history}>
+        <Switch>
+            <Route exact={true} path='/' component={App} />
+            <Route path='/jokes' component={Jokes} />
+        </Switch>
+    </Router>, document.querySelector('#root'))
