7d1bcd7c4a8825c39a17c44b57c3432e791186b3
Abhishek Mukherjee <mukherjeea2017@gmail.com>
2022-09-15 17:24:28 +0530

    Updates

---
+++ precache-manifest.4dfbd4a32281d2d9f6f0b9dad668cda6.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b5e29437a2502014fb00dce501d986a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "efc940e8294ef8deb6cb",
+    "url": "/static/css/2.2a745774.chunk.css"
+  },
+  {
+    "revision": "5fd499ce2eb1f0b4ae5f",
+    "url": "/static/css/main.3c7b17af.chunk.css"
+  },
+  {
+    "revision": "efc940e8294ef8deb6cb",
+    "url": "/static/js/2.7b94f795.chunk.js"
+  },
+  {
+    "revision": "b8a4a246c9108d6044c836e50490298c",
+    "url": "/static/js/2.7b94f795.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fd499ce2eb1f0b4ae5f",
+    "url": "/static/js/main.59299534.chunk.js"
+  },
+  {
+    "revision": "430f72daf7c3a4d42642",
+    "url": "/static/js/runtime-main.351e9c47.js"
+  },
+  {
+    "revision": "67af05e12311bac06327b43ca31e166b",
+    "url": "/static/media/earth.67af05e1.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "/static/media/fontawesome-webfont.acf3dcb7.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "768998c65ce07675ede8536017d4f6ce",
+    "url": "/static/media/image1.768998c6.jpg"
+  },
+  {
+    "revision": "8de6195d8faafe31ac0a842eb0cbe1a9",
+    "url": "/static/media/image2.8de6195d.jpg"
+  },
+  {
+    "revision": "28470933ba170d7cd8d5495969722a5d",
+    "url": "/static/media/image3.28470933.jpg"
+  },
+  {
+    "revision": "0b37f43494ce5a35f3e1575612048750",
+    "url": "/static/media/image4.0b37f434.jpg"
+  },
+  {
+    "revision": "8d16c4aff0e7bd3db1b8bab9619aea9c",
+    "url": "/static/media/image5.8d16c4af.jpg"
+  },
+  {
+    "revision": "62b7cc7591147260c113823ded8f6901",
+    "url": "/static/media/image6.62b7cc75.jpg"
+  },
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "/static/media/ionicons.05acfdb5.woff"
+  },
+  {
+    "revision": "24712f6c47821394fba7942fbb52c3b2",
+    "url": "/static/media/ionicons.24712f6c.ttf"
+  },
+  {
+    "revision": "2c2ae068be3b089e0a5b59abb1831550",
+    "url": "/static/media/ionicons.2c2ae068.eot"
+  },
+  {
+    "revision": "c037dbbc0e6790f30e824a50010df5fb",
+    "url": "/static/media/ionicons.c037dbbc.svg"
+  },
+  {
+    "revision": "b6766dd95386391655c357412f71453f",
+    "url": "/static/media/myImage.b6766dd9.png"
+  }
+]);
